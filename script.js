278bc1544ad02d5121faa52b8ab362e125b102a9
Ravi Prakash <raviprakashdbg21@gmail.com>
2025-07-18 07:06:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 let Button = document.querySelector(".darkmode");
 let Mode = document.querySelector("body");
-let Text1 = document.querySelector("h2");
-let Text2 = document.querySelector("h3");
+let headings = document.querySelectorAll("h2, h3, h4,i");
 let nav = document.querySelector(".navbar");
-let value = 0;
-Button.addEventListener("click",function(){
-    if(value == 0){
 
-        Mode.style.background = "linear-gradient(to right, #ffe0c3, #ffffff)";
-        Button.style.color="teal";
-        Text1.style.color = "teal";
-        Text2.style.color = "teal";
-        nav.style.color="teal";
-        nav.style.textShadow ="none";
+let value = 0;
+
+Button.addEventListener("click", function () {
+    if (value == 0) {
+        // Light Mode
+        Mode.style.backgroundColor = "white";
+        Button.style.color = "teal";
+        nav.style.color = "teal";
+        headings.forEach(h => h.style.color = "teal");
         value = 1;
+    } else {
+        // Dark Mode
+        Mode.style.backgroundColor = "black";
+        Button.style.color = "white";
+        nav.style.color = "white";
+        headings.forEach(h => h.style.color = "white");
+        value = 0;
     }
-    else{
-        Mode.style.background = "linear-gradient(to right , black,indigo 35%, black)";
-        Button.style.color="white";
-        Text1.style.color="white";
-        Text2.style.color="white";
-        nav.style.color= "white";
-        value = 0;   
-    }
-})
+});
